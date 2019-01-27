e72b6ffb7eb51f80cc228d5ef0661513f76c59c1
Jha-Sudhir <sudhirjha.gs@gmail.com>
2019-01-27 21:46:06 +0100

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Product.create[name: "City Bike", description: "abc", image_url: "rental_bike2.jpg", colour: "Red", price: 45]
-Product.create[name: "Mountain Bike", description: "ab22c", image_url: "rental_bike1.jpg", colour: "Red", price: 445]
+Product.create[["name", "City Bike"], ["description", "xdxd"], ["image_url", "rental_bike2.jpg"], ["created_at", "2019-01-27 20:43:45.788415"], ["updated_at", "2019-01-27 20:43:45.788415"], ["colour", "Red"], ["price", "1000.0"]]
 
-Product.create[name: "Heavy Bike", description: "abc222", image_url: "rental_bike3.jpg", colour: "Red", price: 4445]
+#Product.create[name: "Mountain Bike", description: "ab22c", image_url: "rental_bike1.jpg", colour: "Red", price: 445]
+
+#Product.create[name: "Heavy Bike", description: "abc222", image_url: "rental_bike3.jpg", colour: "Red", price: 4445]
 
 Order.create(user_id: 1, product_id: 1, total: 10.0)
 Order.create(user_id: 1, product_id: 2, total: 5.0)
